d2417b3ca660ed0f462f15aaaaf1e16743aaa147
shivangi-dixit <shivangi.dixit02@gmail.com>
2021-03-26 11:34:32 +0530

    Color change

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
 
 const MaterialComponents = [
   BrowserAnimationsModule,
@@ -15,6 +16,7 @@ const MaterialComponents = [
   MatTableModule,
   MatDividerModule,
   MatProgressSpinnerModule,
+  MatDialogModule
 ];
 
 @NgModule({
